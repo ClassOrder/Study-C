7488a7dea3b2fabd61d64390e6f194da9de58ac2
Arth <gift8128@naver.com>
2017-11-26 20:25:10 +0900

    Update

--- BaseStudy/main.c
+++ BaseStudy/main.c
@@ -2,14 +2,14 @@
 //  main.c
 //  BaseStudy
 //
-//  Created by Arthur on 2017. 11. 26..
+//  Created by Emrys on 2017. 11. 26..
 //  Copyright © 2017년 Emrys. All rights reserved.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     printf("Hello, World!\n");
+    printf("Hello, new World!");
     return 0;
 }
