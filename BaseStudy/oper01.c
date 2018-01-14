66528988bfa1c11fbdf5c9f75f9771a7592413a6
Emrys <32980429+GodfatherEmrys@users.noreply.github.com>
2018-01-14 18:30:09 +0900

    C 연산자

---
+++ BaseStudy/oper01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int nTotal;
+
+    nTotal +=1;
+    nTotal +=2;
+    nTotal +=3;
+
+    printf("%d\n", nTotal);
+    return 0;
+}
+
+// 원하는 숫자가 나오지 않는 이유!
